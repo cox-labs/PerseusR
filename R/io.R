269f90901f2e1fa7c4ee98e00bd323c3efe41caa
Yu <shyu@w2k.biochem.mpg.de>
2018-08-29 00:55:27 +0200

    test imputation

--- R/io.R
+++ R/io.R
@@ -110,7 +110,7 @@ read.perseus.default <- function(con, check = TRUE) {
 #  colClasses <- map_perseus_types(types, .typeMap)
   dfCheck <- utils::read.table(conCheck, header = TRUE,
                           sep = '\t', comment.char = '#')
-  if (grepl(';', df[1, 1])){
+  if (grepl(';', dfCheck[1, 1])){
     colClasses <- map_perseus_types(types, .typeMapAddition)
   } else {
     colClasses <- map_perseus_types(types, .typeMapNormal)
